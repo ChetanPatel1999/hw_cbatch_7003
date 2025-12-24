ba485d057139b92f5c15d86a9c91ae7d0f570ff8
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-12-24 20:17:00 +0530

    12/22

---
+++ 59_for.c
@@ -0,0 +1,9 @@
+//its run infinite time
+#include <stdio.h>
+void main()
+{
+    for (;;)
+    {
+        printf("my dear students please complete your task on time\n");
+    }
+}
---
+++ 60_for.c
@@ -0,0 +1,11 @@
+// wap to print a msg 5 times.
+#include <stdio.h>
+void main()
+{
+    int i;
+    for (i = 5; i <= 12; i++) // 6
+    {
+        printf("my dear students \n"); // 1
+        printf("how are you ? \n");
+    }
+}
