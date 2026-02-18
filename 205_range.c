902e638bc49bcda10b48349a4f4f696da794dcbe
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-18 20:22:12 +0530

    16/02

---
+++ 205_range.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    // unsigned char ch;
+    // ch = -45;
+    // printf("ch", ch);
+
+    // unsigned short int a = 65536;
+    // printf("%d", a);
+    unsigned int a = 65536;
+    printf("%d", a);
+}
